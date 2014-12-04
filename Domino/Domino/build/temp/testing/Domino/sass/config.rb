5407cb916dd94b8ddf61e92e372d66f9c167a81e
Marco Zimello <marco.zimello@tecsisnet.it>
2014-12-04 14:15:19 +0100

    removed build dir

--- Domino/Domino/build/temp/testing/Domino/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
-require '..\..\..\..\..\sass\config.rb'
-cache_path = 'E:\Dati\Svil\Apps\Sencha\Corso-Sencha\Domino\Domino/build/.sass-cache'
--- Domino/Domino/build/temp/testing/Domino/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require E:\Dati\Svil\Apps\Sencha\Corso-Sencha\Domino\Domino\app.js
